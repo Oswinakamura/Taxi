f5d3a5dea86bc755afa1562f1e18c98683b41f6f
Oswaldo Balceiro Blanco <bellaco012@hotmail.com>
2020-02-24 15:38:44 -0500

    Mejoramos el TaxiController

---
+++ Taxi.Web/Controllers/TaxiController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+using Taxi.Web.Data;
+using Taxi.Web.Data.Entities;
+
+namespace Taxi.Web.Controllers
+{
+    public class TaxiController : Controller
+    {
+        private readonly DataContext _context;
+
+        public TaxiController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Taxis.ToListAsync());
+        }
+
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taxiEntity = await _context.Taxis.FindAsync(id);
+
+            if (taxiEntity == null)
+            {
+                return NotFound();
+            }
+
+            return View(taxiEntity);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(TaxiEntity taxiEntity)
+        {
+            if (ModelState.IsValid)
+            {
+                taxiEntity.Plaque = taxiEntity.Plaque.ToUpper();
+                _context.Add(taxiEntity);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(taxiEntity);
+        }
+
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taxiEntity = await _context.Taxis.FindAsync(id);
+            if (taxiEntity == null)
+            {
+                return NotFound();
+            }
+            return View(taxiEntity);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, TaxiEntity taxiEntity)
+        {
+            if (id != taxiEntity.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                taxiEntity.Plaque = taxiEntity.Plaque.ToUpper();
+                _context.Update(taxiEntity);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+            return View(taxiEntity);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var taxiEntity = await _context.Taxis.FindAsync(id);
+
+            if (taxiEntity == null)
+            {
+                return NotFound();
+            }
+
+            _context.Taxis.Remove(taxiEntity);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
