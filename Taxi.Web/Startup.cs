fe1b5238a0b3085c8efccda804b544d202748bcd
Oswaldo Balceiro Blanco <bellaco012@hotmail.com>
2020-02-27 21:03:50 -0500

    Add unique index validation

--- Taxi.Web/Startup.cs
+++ Taxi.Web/Startup.cs
@@ -36,6 +36,8 @@ namespace Taxi.Web
                 cfg.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddTransient<SeedDb>();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
